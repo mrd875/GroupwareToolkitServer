e9c2308a029346268d7df70d96e9d8a7615396da
Your Name <you@example.com>
2020-07-29 21:16:41 +0000

    Test case.

---
+++ test/index.js
@@ -0,0 +1,34 @@
+const assert = require('chai').assert
+const GT = require('gt-client')
+const io = require('../src/io')
+const consola = require('consola')
+
+const servUrl = 'http://localhost:3000'
+
+describe('Server testing', () => {
+  before(done => {
+    this.server = io.listen(3000)
+    consola.log('Server listening...')
+    done()
+  })
+
+  after(done => {
+    consola.log('Closing server...')
+    this.server.close()
+    done()
+  })
+
+  it('Can connect', async function () {
+    this.gt = new GT(servUrl)
+
+    assert(!this.gt.isConnected())
+    assert(!this.gt.isAuthed())
+    assert(!this.gt.isInRoom())
+
+    await this.gt.connect()
+
+    assert(this.gt.isConnected())
+    assert(!this.gt.isAuthed())
+    assert(!this.gt.isInRoom())
+  })
+})
--- test/test.js
+++
@@ -1,7 +0,0 @@
-const assert = require('chai').assert
-
-describe('test', () => {
-  it('test', () => {
-    assert.equal('1', 1)
-  })
-})
